b5565afac40d38ffcd60ad60dd744aef2e1a7e2a
gchin108 <130980531+gchin108@users.noreply.github.com>
2024-07-06 03:54:31 -0700

    update env ready for production

--- db/db.ts
+++ db/db.ts
@@ -1,6 +1,11 @@
-import { drizzle } from "drizzle-orm/postgres-js";
+// import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema";
 import postgres from "postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
+import * as dotenv from "dotenv";
 
-const client = postgres(process.env.DATABASE_URL as string);
-export const db = drizzle(client, { schema, logger: false });
+dotenv.config({ path: ".env" });
+// const client = postgres(process.env.DATABASE_URL as string);
+// export const db = drizzle(client, { schema, logger: false });
+export const db = drizzle(sql, { schema, logger: false });
